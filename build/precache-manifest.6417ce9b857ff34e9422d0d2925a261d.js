89a55a6efc175c2d6925faf1258176d9d7b298ec
Majd Warrak <majdwarrrak@gmail.com>
2020-09-29 01:25:33 +0200

    first commit

---
+++ build/precache-manifest.6417ce9b857ff34e9422d0d2925a261d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "722b6f50cbfe9f37b6b2c7f2935fb32a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c466c03b43fe0c5d3a4",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "4e2787688cc696011c72",
+    "url": "/static/css/main.bc9a6469.chunk.css"
+  },
+  {
+    "revision": "1c466c03b43fe0c5d3a4",
+    "url": "/static/js/2.f7b51e6c.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.f7b51e6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e2787688cc696011c72",
+    "url": "/static/js/main.4e9695be.chunk.js"
+  },
+  {
+    "revision": "d2413320a277bb2c659c",
+    "url": "/static/js/runtime-main.c14436a3.js"
+  }
+]);
